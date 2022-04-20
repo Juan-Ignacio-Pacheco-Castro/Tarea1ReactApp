fcf76dd1b6ebb23474b1da33984839b4fb13a0ce
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-04-20 15:51:08 -0600

    Nombre y Carnet agregados a App

---
+++ my-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Juan Ignacio Pacheco Castro
+      B85841
+    </h1>
+  )
+}
+
+//export default App;
