da69cfdb195db5fe24e3d2ab6c1b443800ce3e97
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2022-04-20 20:27:33 -0600

    Saque el proyecto de la carpeta my-proyect

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <h1 className="text-3xl font-bold underline">
+                    Juan Ignacio Pacheco Castro (B85841)
+                </h1>
+            </header>
+        </div>
+    )
+}
